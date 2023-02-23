528ba7e0a5a6de53d36d0f19450c53691b88f8b3
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-02-22 23:20:13 -0300

    feat: botao que sobe a pagina

---
+++ scroll.js
@@ -0,0 +1,9 @@
+$('.sobe').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'), 
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset
+    }, 1000)
+    
+})
