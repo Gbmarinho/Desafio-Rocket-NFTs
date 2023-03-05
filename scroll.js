9ce92633e9d5b8d06b4fe350424f09ea438eaa8b
Gabriel Marinho <gabriel.marinho@injunior.com.br>
2023-03-05 18:07:53 -0300

    fix: bug no background

--- scroll.js
+++ scroll.js
@@ -7,3 +7,20 @@ $('.sobe').click(function(e){
     }, 1000)
     
 })
+
+var cont2 = 0;
+$('#btn-menu').click(function(e){
+    if(cont2 % 2 == 0){
+        
+        $('.menu-escondido').css("transform", "scaleY(1)");
+    }else{
+
+        $('.menu-escondido').css("transform", "scaleY(0)");
+    }
+    cont2++; 
+})
+
+function animar(){
+    const btn = document.getElementById("btn-menu");
+    btn.classList.toggle('ativar');
+}
